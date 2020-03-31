754b1f8c51770d6e929f6a9dc74ea0788cff5569
zhudianyu <1462415060@qq.com>
2020-03-31 14:20:58 +0800

    完成texture练习

--- Texture/Texture.cpp
+++ Texture/Texture.cpp
@@ -6,6 +6,32 @@
 #include <SOIL.h>
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+GLuint GenTexture(const char* path)
+{
+	int width, height;
+	unsigned char* image = SOIL_load_image(path, &width, &height, 0, SOIL_LOAD_RGB);
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+	glActiveTexture(GL_TEXTURE0);
+
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	//纹理环绕
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	//纹理放大缩小
+// 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+// 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	//多级渐进纹理采样 纹理放大不会使用多级渐远纹理
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D,0);
+	return textureID;
+}
 int main()
 {
 	glfwInit();
@@ -28,9 +54,11 @@ int main()
 	}
 
 	GLfloat vertices[] = {
-	0.5,-0.5,0, 1,0,0,
-	-0.5,-0.5,0, 0,1,0,
-	0,0.5,0, 0,0,1
+   //位置       //color //coord
+	0.5,0.5,0, 1,0,0,   1,1,
+	0.5,-0.5,0, 0,1,0,  1,0,
+	-0.5,-0.5,0, 0,0,1,  0,0,
+	-0.5,0.5,0,  1,1,0,  0,1
 	};
 
 	GLfloat texCoords[] = {
@@ -39,24 +67,35 @@ int main()
 		0.5f,1.0f
 	};
 
-	int width, height;
-	unsigned char* image = SOIL_load_image("../Texture/wall.jpg", &width, &height, 0, SOIL_LOAD_RGB);
 
+
+	GLuint textureID = GenTexture("../Texture/wall.jpg");
+	GLuint faceTextureID = GenTexture("../Texture/awesomeface.png");
 	Shader ourShader("../Texture/default.vs", "../Texture/default.frag");
 
+	GLuint indices[] = {
+		0,1,3,
+		1,2,3
+	};
 
-	GLuint VBO, VAO;
+	GLuint VBO, VAO,EBO;
 	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
 	glBindVertexArray(VAO);
 
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (GLvoid*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GLfloat)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)(6 * sizeof(GL_FLOAT)));
+	glEnableVertexAttribArray(2);
 	//解绑vao
 	glBindVertexArray(0);
 
@@ -67,18 +106,23 @@ int main()
 		glClearColor(0.2, 0.3, 0.3, 1);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		GLint vertexOffsetLocation = glGetUniformLocation(ourShader.Program, "offset");
-
 		ourShader.Use();
-		//glUniform2f(vertexOffsetLocation, 0.5f, 0);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture1"), 0);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, faceTextureID);
+		glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture2"), 1);
+
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6,GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 	}
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glfwTerminate();
 
 	return 0;
--- Texture/default.vs
+++ Texture/default.vs
@@ -1,13 +1,15 @@
 #version 330 core
 layout(location = 0) in vec3 position;
 layout(location = 1) in vec3 color;
+layout(location = 2) in vec2 texCoord;
 uniform vec2 offset;
 out vec3 ourColor;
-
+out vec2 TexCoord;
 void main()
 {
   vec3 findal = position;
 
   gl_Position = vec4(position,1.0f);
   ourColor = color;
+  TexCoord = texCoord;
 }
