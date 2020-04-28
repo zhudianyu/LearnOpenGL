90adc4007013b48484f674679ead0a15770ad348
zhudianyu <1462415060@qq.com>
2020-04-28 18:47:11 +0800

    022_NormalMap 完成法线贴图的学习

--- 021_MSAA/021_MSAA.cpp
+++ 021_MSAA/021_MSAA.cpp
@@ -78,6 +78,73 @@ GLuint GenAlphaTexture(const char* path)
 	glBindTexture(GL_TEXTURE_2D, 0);
 	return textureID;
 }
+GLenum glCheckError_(const char* file, int line)
+{
+	GLenum errorCode;
+	while ((errorCode = glGetError()) != GL_NO_ERROR)
+	{
+		std::string error;
+		switch (errorCode)
+		{
+		case GL_INVALID_ENUM:                  error = "INVALID_ENUM"; break;
+		case GL_INVALID_VALUE:                 error = "INVALID_VALUE"; break;
+		case GL_INVALID_OPERATION:             error = "INVALID_OPERATION"; break;
+		case GL_STACK_OVERFLOW:                error = "STACK_OVERFLOW"; break;
+		case GL_STACK_UNDERFLOW:               error = "STACK_UNDERFLOW"; break;
+		case GL_OUT_OF_MEMORY:                 error = "OUT_OF_MEMORY"; break;
+		case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
+		}
+		std::cout << error << " | " << file << " (" << line << ")" << std::endl;
+	}
+	return errorCode;
+}
+#define glCheckError() glCheckError_(__FILE__, __LINE__)
+
+void APIENTRY glDebugOutput(GLenum source,
+	GLenum type,
+	GLuint id,
+	GLenum severity,
+	GLsizei length,
+	const GLchar* message,
+	const void* userParam)
+{
+	if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return; // ignore these non-significant error codes
+
+	std::cout << "---------------" << std::endl;
+	std::cout << "Debug message (" << id << "): " << message << std::endl;
+
+	switch (source)
+	{
+	case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
+	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
+	case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
+	case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
+	case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
+	case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
+	} std::cout << std::endl;
+
+	switch (type)
+	{
+	case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
+	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
+	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break;
+	case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
+	case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
+	case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
+	case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
+	case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
+	case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+	} std::cout << std::endl;
+
+	switch (severity)
+	{
+	case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
+	case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
+	case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
+	case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
+	} std::cout << std::endl;
+	std::cout << std::endl;
+}
 GLFWwindow* InitGL()
 {
 	glfwInit();
@@ -85,7 +152,7 @@ GLFWwindow* InitGL()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE); // comment this line in a release build! 
 	//开启多重采样
 	glfwWindowHint(GLFW_SAMPLES, 4);
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, projName.c_str(), nullptr, nullptr);
@@ -106,9 +173,21 @@ GLFWwindow* InitGL()
 	}
 	return window;
 }
+
 int main()
 {
 	GLFWwindow* window = InitGL();
+
+	// enable OpenGL debug context if context allows for debug context
+	GLint flags; 
+	glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+	if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
+	{
+		glEnable(GL_DEBUG_OUTPUT);
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS); // makes sure errors are displayed synchronously
+		glDebugMessageCallback(glDebugOutput, nullptr);
+		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+	}
 	//把每一个片段着色器的结果 存储在个采样点上 最后对采样点的结果进行平均
 	glEnable(GL_MULTISAMPLE);
 	GLfloat cubeVertices[] = {
@@ -166,7 +245,7 @@ int main()
 	 1.0f,  1.0f,  1.0f, 1.0f
 	};
 	glEnable(GL_DEPTH_TEST);
-
+	GLenum errorNo = glGetError();
 	// Setup cube VAO
 	GLuint cubeVAO, cubeVBO;
 	glGenVertexArrays(1, &cubeVAO);
@@ -240,7 +319,7 @@ int main()
 		cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << endl;
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 	//解绑fbo
-	glBindRenderbuffer(GL_FRAMEBUFFER, 0);
+//	glBindRenderbuffer(GL_FRAMEBUFFER, 0);
 
 	// configure second post-processing framebuffer
 	unsigned int intermediateFBO;
@@ -313,6 +392,7 @@ int main()
 
 		 // 1. draw scene as normal in multisampled buffers
 		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+	//	std::cout  << glCheckError() << endl;
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glEnable(GL_DEPTH_TEST);
@@ -327,23 +407,26 @@ int main()
 		glBindVertexArray(cubeVAO);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 
-// 		// 2. now blit multisampled buffer(s) to normal colorbuffer of intermediate FBO. Image is stored in screenTexture
-		glBindFramebuffer(GL_READ_FRAMEBUFFER, framebuffer);
-		glBindFramebuffer(GL_DRAW_FRAMEBUFFER, intermediateFBO);
-		glBlitFramebuffer(0, 0, WIDTH, HEIGHT, 0, 0, WIDTH, HEIGHT, GL_COLOR_BUFFER_BIT, GL_NEAREST);
-// 
-// 		// 3. now render quad with scene's visuals as its texture image
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		glDisable(GL_DEPTH_TEST);
-// 
-// 		// draw Screen quad
- 		screenShader.Use();
- 		glBindVertexArray(quadVAO);
- 		glActiveTexture(GL_TEXTURE0);
- 		glBindTexture(GL_TEXTURE_2D, screenTexture); // use the now resolved color attachment as the quad's texture
- 		glDrawArrays(GL_TRIANGLES, 0, 6);
+		//// 2. now blit multisampled buffer(s) to normal colorbuffer of intermediate FBO. Image is stored in screenTexture
+ 	//			glBindFramebuffer(GL_READ_FRAMEBUFFER, framebuffer);
+ 	//		//	std::cout << "2 " << glCheckError() << endl;
+ 	//			glBindFramebuffer(GL_DRAW_FRAMEBUFFER, intermediateFBO);
+ 	//			//std::cout << "3 " << glCheckError() << endl;
+ 	//			glBlitFramebuffer(0, 0, WIDTH, HEIGHT, 0, 0, WIDTH, HEIGHT, GL_COLOR_BUFFER_BIT, GL_NEAREST);
+ 	//	
+ 	//			// 3. now render quad with scene's visuals as its texture image
+ 	//			glBindFramebuffer(GL_FRAMEBUFFER, 0);
+ 	//			//std::cout << "4 " << glCheckError() << endl;
+ 	//			glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+ 	//			glClear(GL_COLOR_BUFFER_BIT);
+ 	//			glDisable(GL_DEPTH_TEST);
+ 	//	
+ 	//			// draw Screen quad
+ 	//	 		screenShader.Use();
+ 	//	 		glBindVertexArray(quadVAO);
+ 	//	 		glActiveTexture(GL_TEXTURE0);
+ 	//	 		glBindTexture(GL_TEXTURE_2D, screenTexture); // use the now resolved color attachment as the quad's texture
+ 	//	 		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 
 		glfwSwapBuffers(window);
